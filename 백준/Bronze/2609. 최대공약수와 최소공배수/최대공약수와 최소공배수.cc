e837ce33df63176347d3182d49f13214edd760af
Jeong UiGwon <93179018+JeongUiGwon@users.noreply.github.com>
2022-09-18 19:51:54 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 220 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.cc
@@ -0,0 +1,52 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <vector>
+#define INF 987654321
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int a, b;
+	cin >> a >> b;
+
+	int min_num, max_num;
+	if (a > b)
+	{
+		max_num = a;
+		min_num = b;
+	}
+	else
+	{
+		max_num = b;
+		min_num = a;
+	}
+
+	int gcf = 1;
+	for (int i = 1; i <= min_num; i++)
+	{
+		if (a % i == 0 && b % i == 0)
+		{
+			gcf = i;
+		}
+	}
+
+	int lcm = 1;
+	for (int i = max_num; i < INF; i++)
+	{
+		if (i % a == 0 && i % b == 0)
+		{
+			lcm = i;
+			break;
+		}
+	}
+
+	cout << gcf << '\n' << lcm;
+
+	return 0;
+}
