0e6f3d122c548e8201b12ced5d489be755009d26
Jeong UiGwon <93179018+JeongUiGwon@users.noreply.github.com>
2022-09-28 22:02:55 +0900

    [Gold IV] Title: DSLR, Time: 3628 ms, Memory: 2316 KB -BaekjoonHub

--- 백준/Gold/9019. DSLR/DSLR.cc
+++ 백준/Gold/9019. DSLR/DSLR.cc
@@ -2,80 +2,79 @@
 #include <iostream>
 #include <string>
 #include <cstring>
-
 using namespace std;
 
-int a, b;
-bool visited[10000];
+int A, B;
+int visited[10000];
 
-void bfs()
-{
-    queue<pair<int, string>> q;
-    q.push(make_pair(a, ""));
-    visited[a] = true;
+void bfs() {
+	queue<pair<int, string>> q;
+	q.push({ A, ""});
+	visited[A] = 1;
 
-    while (!q.empty())
-    {
-        int cur_num = q.front().first;
-        string cur_op = q.front().second;
-        q.pop();
+	while (!q.empty())
+	{
+		int now_num = q.front().first;
+		string now_path = q.front().second;
+		q.pop();
 
-        if (cur_num == b)
-        {
-            cout << cur_op << '\n';
-            return;
-        }
+		if (now_num == B)
+		{
+			cout << now_path << '\n';
+			return;
+		}
 
-        int D, S, L, R, temp;
-        // D 연산
-        D = (cur_num * 2) % 10000;
-        if (!visited[D])
-        {
-            visited[D] = true;
-            q.push(make_pair(D, cur_op + "D"));
-        }
+		int next_num = (now_num * 2) % 10000;
 
-        // S 연산
-        S = cur_num - 1 < 0 ? 9999 : cur_num - 1;
-        if (!visited[S])
-        {
-            visited[S] = true;
-            q.push(make_pair(S, cur_op + "S"));
-        }
+		if (!visited[next_num]) 
+		{
+			visited[next_num] = 1;
+			q.push({ next_num , now_path + "D" });
+		}
 
-        // L 연산
-        L = (cur_num % 1000) * 10 + (cur_num / 1000);
-        if (!visited[L])
-        {
-            visited[L] = true;
-            q.push(make_pair(L, cur_op + "L"));
-        }
+		next_num = now_num - 1 < 0 ? 9999 : now_num - 1;
 
-        // R 연산
-        R = cur_num / 10 + (cur_num % 10) * 1000;
-        if (!visited[R])
-        {
-            visited[R] = true;
-            q.push(make_pair(R, cur_op + "R"));
-        }
-    }
+		if (!visited[next_num])
+		{
+			visited[next_num] = 1;
+			q.push({ next_num , now_path + "S" });
+		}
+
+		next_num = (now_num % 1000) * 10 + (now_num / 1000);
+
+		if (!visited[next_num])
+		{
+			visited[next_num] = 1;
+			q.push({ next_num , now_path + "L" });
+		}
+
+		next_num = (now_num % 10) * 1000 + (now_num / 10);
+
+		if (!visited[next_num])
+		{
+			visited[next_num] = 1;
+			q.push({ next_num , now_path + "R" });
+		}
+	}
 }
 
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 
-    int T;
-    cin >> T;
+	int T;
+	cin >> T;
 
-    while (T--)
-    {
-        cin >> a >> b;
-        memset(visited, false, sizeof(visited)); // 초기화
-        bfs();
-    }
+	for (int tc = 0; tc < T; tc++)
+	{
+		memset(visited, 0, sizeof(visited));
 
-    return 0;
+		cin >> A >> B;
+
+		bfs();
+	}
+
+	return 0;
 }
