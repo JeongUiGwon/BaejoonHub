e81d64d9d2cba6d5746925cf7ef97a914e52de10
Jeong UiGwon <93179018+JeongUiGwon@users.noreply.github.com>
2023-06-05 13:18:07 +0900

    [Silver IV] Title: solved.ac, Time: 32 ms, Memory: 3192 KB -BaekjoonHub

--- 백준/Silver/18110. solved．ac/solved．ac.cc
+++ 백준/Silver/18110. solved．ac/solved．ac.cc
@@ -1,40 +1,54 @@
-#include<stdio.h>
-#include<vector>
-#include<algorithm>
-#pragma warning(disable:4996)
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
 using namespace std;
-int main()
+
+int main() 
 {
-    vector <int> vec;
-    int N;
-    int number;
-    scanf("%d", &N);
-    if (N == 0)
-    {
-        printf("0");
-        return 0;
-    }
-    for (int i = 0; i < N; i++)
-    {
-        scanf("%d", &number);
-        vec.push_back(number);
-    }    
-    sort(vec.begin(), vec.end()); // 정렬 
-    double num;
-    num = (double)N * 15 / 100;
-    int value = num;
-    if (num - value >= 0.5)
-        value++;
-    int cnt = 0;
-    int sum = 0;
-    for (int i = value; i < N - value; i++)
-    {
-        sum = vec[i] + sum;
-        cnt++;
-    }
-    double avg = (double)sum/(double)cnt;
-    int avg_2=avg;
-    if (avg - avg_2 >= 0.5)
-        avg_2++;
-    printf("%d", avg_2);
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n;
+	double res = 0;
+
+	cin >> n;
+
+	if (n == 0)
+	{
+		cout << res;
+		return 0;
+	}
+
+	vector<int> numbers(n);
+
+	for (int i = 0; i < n; i++)
+		cin >> numbers[i];
+
+	sort(numbers.begin(), numbers.end());
+
+	double idx = (double)n * 0.15;
+	int val = idx;
+
+	if (idx - val >= 0.5) val++;
+
+	int cnt = 0;
+
+	for (int i = val; i < n - val; i++)
+	{
+		res += numbers[i];
+		cnt++;
+	}
+
+	res /= (double)(cnt);
+	int res2 = res;
+
+	if ((res - res2) >= 0.5) res2++;
+
+	cout << res2;
+
+	return 0;
 }
