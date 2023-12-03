3a99b74e58074bbefca16bd05b9e7ba34862b037
Jeong UiGwon <93179018+JeongUiGwon@users.noreply.github.com>
2023-12-03 14:02:35 +0900

    [Gold III] Title: 앱, Time: 4 ms, Memory: 5968 KB -BaekjoonHub

--- 백준/Gold/7579. 앱/앱.cc
+++ 백준/Gold/7579. 앱/앱.cc
@@ -1,28 +1,11 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
 
 using namespace std;
 
+int memory_table[101];
+int cost_table[101];
 int dp[101][10001];
-int memory[101];
-int cost[101];
-
-
-// 5 60
-// 30	10	20	35	40
-// 3	0	3	5	4
-
-/*
-		0	1	2	3	4	5
-	0
-	1	0	
-	2	0	0	3	3	3	INF	INF	
-	3	0	0	3	3	6	6	6
-	4	0	0	3	3	6	6	6
-	5	0	0	
-*/
-
 
 int main()
 {
@@ -30,35 +13,34 @@ int main()
 	cin.tie(0);
 	cout.tie(0);
 
-	int N, M, max_cost = 0;
+	int N, M, sum_cost = 0;
 	cin >> N >> M;
 
 	for (int i = 1; i <= N; i++)
-		cin >> memory[i];
+		cin >> memory_table[i];
 
 	for (int i = 1; i <= N; i++)
 	{
-		cin >> cost[i];
-		max_cost += cost[i];
+		cin >> cost_table[i];
+		sum_cost += cost_table[i];
 	}
 
-
 	for (int i = 1; i <= N; i++)
 	{
-		for (int j = 1; j <= max_cost; j++)
+		for (int j = 0; j <= sum_cost; j++)
 		{
-			if (j - cost[i] >= 0)
-				dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - cost[i]] + memory[i]);
-			else
-				dp[i][j] = dp[i - 1][j];
+			if (j - cost_table[i] >= 0)
+				dp[i][j] = max(dp[i][j], dp[i - 1][j - cost_table[i]] + memory_table[i]);
+
+			dp[i][j] = max(dp[i][j], dp[i - 1][j]);
 		}
 	}
 
-	for (int i = 0; i <= max_cost; i++)
+	for (int i = 0; i <= sum_cost; i++)
 	{
 		if (dp[N][i] >= M)
 		{
-			cout << i;
+			cout << i << endl;
 			break;
 		}
 	}
