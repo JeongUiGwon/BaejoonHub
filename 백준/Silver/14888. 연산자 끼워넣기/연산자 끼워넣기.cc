9fe7cec79de3889bcd395ef3bfa9f9fd2072cdaf
Jeong UiGwon <93179018+JeongUiGwon@users.noreply.github.com>
2024-04-06 17:05:43 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.cc
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.cc
@@ -10,53 +10,31 @@ int max_val = -1000000000;
 int min_val = 1000000000;
 int numbers[MAX_N];
 int num_operator[4];
-vector<int> order_operator;
 
-int calculate()
-{
-	int ret = numbers[0];
 
-	for (int i = 0; i < order_operator.size(); i++) {
-		int next_num = numbers[1 + i];
-
-		switch (order_operator[i])
-		{
-		case 0:
-			ret += next_num;
-			break;
-		case 1:
-			ret -= next_num;
-			break;
-		case 2:
-			ret *= next_num;
-			break;
-		case 3:
-			ret /= next_num;
-			break;
-		default:
-			break;
-		}
-	}
-
-	return ret;
-}
-
-void dfs(int cur)
+void dfs(int cur, int res)
 {
 	if (cur == N - 1) {
-		int val = calculate();
-		max_val = max(max_val, val);
-		min_val = min(min_val, val);
+		max_val = max(max_val, res);
+		min_val = min(min_val, res);
 		return;
 	}
 
 	for (int i = 0; i < 4; i++) {
 		if (num_operator[i] == 0) continue;
 
-		order_operator.push_back(i);
+		int next = cur + 1;
 		num_operator[i]--;
-		dfs(cur + 1);
-		order_operator.pop_back();
+
+		if (i == 0)
+			dfs(next, res + numbers[next]);
+		else if (i == 1)
+			dfs(next, res - numbers[next]);
+		else if (i == 2)
+			dfs(next, res * numbers[next]);
+		else if (i == 3)
+			dfs(next, res / numbers[next]);
+
 		num_operator[i]++;
 	}
 }
@@ -75,7 +53,7 @@ int main()
 	for (int i = 0; i < 4; i++)
 		cin >> num_operator[i];
 
-	dfs(0);
+	dfs(0, numbers[0]);
 
 	cout << max_val << '\n' << min_val;
 }
