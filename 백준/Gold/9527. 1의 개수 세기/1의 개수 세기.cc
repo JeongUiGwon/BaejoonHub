231a458f6396f4dcb3fe151b5956a3a56ea6f067
Jeong UiGwon <93179018+JeongUiGwon@users.noreply.github.com>
2024-04-11 10:33:20 +0900

    [Gold II] Title: 1의 개수 세기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/9527. 1의 개수 세기/1의 개수 세기.cc
@@ -0,0 +1,41 @@
+#include <iostream>
+#define MAX 55
+
+using namespace std;
+
+long long A, B;
+long long power[MAX];
+
+long long calculate(long long x)
+{
+	long long ret = x & 1;
+
+	for (int i = MAX - 1; i > 0; i--)
+	{
+		if (x & (1LL << i))
+		{
+			ret += power[i - 1] + (x - (1LL << i) + 1);
+			x -= 1LL << i;
+		}
+	}
+
+	return ret;
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	power[0] = 1;
+	for (long long i = 1; i < 55; i++)
+		power[i] = power[i - 1] * 2 + (1LL << i);
+
+	cin >> A >> B;
+
+	long long ans = calculate(B) - calculate(A - 1);
+	cout << ans;
+
+	return 0;
+}
