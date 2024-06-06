a28e0b816af616938d83db3254cb6b91d5e4d689
Jeong UiGwon <93179018+JeongUiGwon@users.noreply.github.com>
2024-06-06 18:54:25 +0900

    [Gold III] Title: 사회망 서비스(SNS), Time: 752 ms, Memory: 70652 KB -BaekjoonHub

--- 백준/Gold/2533. 사회망 서비스（SNS）/사회망 서비스（SNS）.cc
+++ 백준/Gold/2533. 사회망 서비스（SNS）/사회망 서비스（SNS）.cc
@@ -7,21 +7,25 @@ using namespace std;
 
 int N;
 vector<int> graph[MAX_N];
-int dp[MAX_N][2];
+int dp[MAX_N][2]; // dp[x][y] = x번 노드가 0(얼리 아답터) 또는 1(일반인) 경우,
 bool visited[MAX_N];
 
 void dfs(int cur)
 {
 	visited[cur] = true;
-	dp[cur][0] = 1;
+	dp[cur][0] = 1;	// 얼리 아답터인 경우 1
 
+	// 1. 현재 노드가 0(얼리 아답터)인 경우, dp[현재 노드][얼리 아답터] += min(dp[다음 탐색 노드][얼리 아답터], dp[다음 탐색 노드][일반인])
+	// 2. 현재 노드가 1(일반인)인 경우, dp[현재 노드][일반인] += dp[다음 탐색 노드][얼리 아답터]
 	for (int i = 0; i < graph[cur].size(); i++) {
 		int next = graph[cur][i];
 		if (visited[next]) continue;
 
-		dfs(next);
-		dp[cur][1] += dp[next][0];
+		dfs(next);	// 다음 노드 탐색
+
+		// 점화식
 		dp[cur][0] += min(dp[next][1], dp[next][0]);
+		dp[cur][1] += dp[next][0];
 	}
 }
 
@@ -31,8 +35,10 @@ int main()
 	cin.tie(NULL);
 	cout.tie(NULL);
 
+	// N : 정점 개수
 	cin >> N;
 
+	// 그래프 입력
 	for (int i = 0; i < N; i++) {
 		int u, v;
 		cin >> u >> v;
@@ -40,8 +46,10 @@ int main()
 		graph[v].push_back(u);
 	}
 
+	// 1번부터 탐색
 	dfs(1);
 
+	// 1번이 얼리 아답터인 경우와 일반인인 경우, 총 2개의 경우 중에서 작은 값 출력
 	cout << min(dp[1][0], dp[1][1]);
 
 	return 0;
